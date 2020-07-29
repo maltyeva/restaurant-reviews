0a2066b0eda61b1b272c57c0fbe314a2b4d32cde
Maria <maria.altyeva@gmail.com>
2020-07-29 18:19:39 +0800

    restaurants and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -12,3 +12,7 @@ Rails.application.routes.draw do
   # resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# /restaurants/new
+# /restaurants/21/restaurants/new
